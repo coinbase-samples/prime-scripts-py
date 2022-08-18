cac128feb2b44e2ecb11607e0897371816dcd97f
Nick Morgan <nick.morgan@coinbase.com>
2022-08-18 14:35:04 -0400

    Update formatting
    This updates formatting in the License header to ensure it is uniform
    across all files. It removes the async for the sign function in
    prime_ws_orders as it is not needed.

--- prime_create_order_preview.py
+++ prime_create_order_preview.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from urllib.parse import urlparse
 import json, hmac, hashlib, time, uuid, os, base64, requests
 
