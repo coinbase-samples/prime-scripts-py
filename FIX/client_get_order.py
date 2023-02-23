c4d0981fb1ac456a1f4a810e0053e1d3e4adbffc
jc-cb <102254382+jc-cb@users.noreply.github.com>
2023-02-23 08:34:41 -0500

    remove sys

--- FIX/client_get_order.py
+++ FIX/client_get_order.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import sys
 import quickfix
 from app.configuration import Configuration
 from build_get_order import BuildGet
