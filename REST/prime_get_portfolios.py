822fcafb0c62c14190a78dcd6aae9226ba001e54
jc-cb <102254382+jc-cb@users.noreply.github.com>
2023-03-29 11:21:13 -0600

    portfolio changes

--- REST/prime_get_portfolios.py
+++ REST/prime_get_portfolios.py
@@ -18,9 +18,8 @@ import json, hmac, hashlib, time, os, base64, requests
 API_KEY = os.environ.get('ACCESS_KEY')
 SECRET_KEY = os.environ.get('SIGNING_KEY')
 PASSPHRASE = os.environ.get('PASSPHRASE')
-PORTFOLIO_ID = os.environ.get('PORTFOLIO_ID')
 
-uri =f'https://api.prime.coinbase.com/v1/portfolios/{PORTFOLIO_ID}'
+uri =f'https://api.prime.coinbase.com/v1/portfolios/'
 timestamp = str(int(time.time()))
 method = 'GET'
 
