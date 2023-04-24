333b982cfa98158202237a07b9f427abf5b8d3da
jc-cb <102254382+jc-cb@users.noreply.github.com>
2023-04-24 15:07:21 -0400

    print change

--- REST/prime_create_allocations.py
+++ REST/prime_create_allocations.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SIGNING_KEY')
 PASSPHRASE = os.environ.get('PASSPHRASE')
 ORIGIN_PORTFOLIO_ID = os.environ.get('PORTFOLIO_ID')
 
-uri = f'https://api.prime.coinbase.com/v1/allocations'
+uri = 'https://api.prime.coinbase.com/v1/allocations'
 timestamp = str(int(time.time()))
 method = 'POST'
 allocation_id = uuid.uuid4()
