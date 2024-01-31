9f42b2d1d499e4c831251c70b93786ec3a98439d
jc-cb <jeff.curry@coinbase.com>
2024-01-31 11:18:44 -0500

    add pagination

--- REST/prime_locate_order_by_client_order_id.py
+++ REST/prime_locate_order_by_client_order_id.py
@@ -12,31 +12,58 @@ SECRET_KEY = os.environ.get('SIGNING_KEY')
 PASSPHRASE = os.environ.get('PASSPHRASE')
 PORTFOLIO_ID = os.environ.get('PORTFOLIO_ID')
 
-uri = f'https://api.prime.coinbase.com/v1/portfolios/{PORTFOLIO_ID}/orders'
-url_path = urlparse(uri).path
-timestamp = str(int(time.time()))
-message = timestamp + 'GET' + url_path
-signature_b64 = base64.b64encode(hmac.digest(SECRET_KEY.encode(), message.encode(), hashlib.sha256))
 
-headers = {
-   'X-CB-ACCESS-SIGNATURE': signature_b64,
-   'X-CB-ACCESS-timestamp': timestamp,
-   'X-CB-ACCESS-KEY': API_KEY,
-   'X-CB-ACCESS-PASSPHRASE': PASSPHRASE,
-   'Accept': 'application/json'
-}
+def list_orders(uri):
+    url_path = urlparse(uri).path
+    timestamp = str(int(time.time()))
+    message = timestamp + 'GET' + url_path
+    signature_b64 = base64.b64encode(hmac.digest(SECRET_KEY.encode(), message.encode(), hashlib.sha256))
 
-response = requests.get(uri, headers=headers)
-parsed_response = json.loads(response.text)
+    headers = {
+       'X-CB-ACCESS-SIGNATURE': signature_b64,
+       'X-CB-ACCESS-timestamp': timestamp,
+       'X-CB-ACCESS-KEY': API_KEY,
+       'X-CB-ACCESS-PASSPHRASE': PASSPHRASE,
+       'Accept': 'application/json'
+    }
 
-# Search for the order with the specified client_order_id
-order_found = False
-for order in parsed_response.get('orders', []):
-    if order.get('client_order_id') == client_order_id_to_check:
-        order_id = order.get('id')
-        print(f'Order found:\n  Client Order ID: {client_order_id_to_check}\n  Associated Order ID: {order_id}')
-        order_found = True
+    response = requests.get(uri, headers=headers)
+    if response.status_code != 200:
+        print(f'Failed to list orders: HTTP {response.status_code} - {response.text}')
+        return None
+    return json.loads(response.text)
+
+
+def search_order(client_order_id, parsed_response):
+    for order in parsed_response.get('orders', []):
+        if order.get('client_order_id') == client_order_id:
+            return order
+    return None
+
+
+base_uri = f'https://api.prime.coinbase.com/v1/portfolios/{PORTFOLIO_ID}/orders'
+uri = base_uri
+
+order_found = None
+
+while True:
+    parsed_response = list_orders(uri)
+    if not parsed_response:
         break
 
-if not order_found:
+    order_found = search_order(client_order_id_to_check, parsed_response)
+    if order_found:
+        break
+
+    pagination_info = parsed_response.get('pagination', {})
+
+    if pagination_info.get('has_next'):
+        next_cursor = pagination_info.get('next_cursor')
+        uri = f'{base_uri}?cursor={next_cursor}'
+    else:
+        break
+
+if order_found:
+    print(f'Order found:\n  Client Order ID: {client_order_id_to_check}\n  Associated Order ID: {order_found.get("id")}')
+else:
     print(f'Order with client_order_id {client_order_id_to_check} not found.')
